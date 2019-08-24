d9e667e5410ca04e797e4297013c3fde572503c2
Parker Stromberg <parker.stromberg@gmail.com>
2019-08-24 11:10:14 -0600

    Added comments and hints

--- Texas Holdem Project/script.js
+++ Texas Holdem Project/script.js
@@ -1,5 +1,6 @@
 // Intialize Suits, Value, and Deck 
 const suits = ['Hearts', 'Spades', 'Clubs', 'Diamond'];
+// Parker: Value is ambiguous to what it contains, and it doesn't imply that it is an array. I would at least rename it to "values".
 let value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 /* // Initializes Deck of Cards and Shuffles the cards
@@ -52,14 +53,19 @@ console.log(dealCards());
 */
 
 // Attempt #2 with Objects
+// Parker: Consider declaring the list of suits and values in this function instead of refering to the parent scope
+// Parker: I think it would be better to name this function "generateDeck", as that is what the majority of the code is doing
 function shuffleDeck() {
     let deck = [];
+    // Parker: tempDeck and tempValue are confusing variable names. With sufficent refactoring, you won't need to even have them
     let tempDeck = [];
     let tempValue = [];
+    // Parker: I would move the card decleration outside of this function
     function Card (suit, value) {
         this.suitName = suit;
         this.cardValue = value;
     }
+    // Parker: You could go without having "suitControl" and "deckIndex", but I will let you figure out how
     let suitControl = 0;
     let deckIndex = 0;
     // Create the card objects and save to the deck array
@@ -71,6 +77,7 @@ function shuffleDeck() {
             value.splice(randomNum, 1);
             
             // Card pulls from suits and newly randomized values and saves card value to deck
+            // Parker: I would rename this to card instead of tempCard
             let tempCard = new Card();
             tempCard.suitName = suits[suitControl];
             tempCard.cardValue = tempValue[i];
@@ -81,22 +88,25 @@ function shuffleDeck() {
         value = tempValue;
     }
 
-    //Shuffles the suits
+    // Shuffles the suits
     for (a = 0; a < 52; a++){
         let randomCard = Math.floor(Math.random() * deck.length);
         tempDeck.push(deck[randomCard]);
         deck.splice(randomCard, 1);
     }
 
+    // Parker: You could just return tempDeck instead of reassigning deck to tempDeck and then returning deck
     deck = tempDeck;
     return deck;
 }
 
 function dealCards() {
+    // Parker: I would rename this to something like "faceUpCards". Especially something that implies that it is an array
     let faceUp = [];
     let playDeck = shuffleDeck();
+
     // 3 cards to the front
-    for(i = 0; i < 3; i++){
+    for (i = 0; i < 3; i++) {
         let randomDraw = Math.floor(Math.random() * playDeck.length);
         faceUp.push(playDeck[randomDraw]);
         playDeck.splice(randomDraw, 1);
@@ -105,3 +115,7 @@ function dealCards() {
 
     return faceUp;
 }
+
+// Parker: After calling dealCards, you would take the return value and generate a ui
+// Parker: You could make a function that takes in an array of cards and spits out dom nodes representing each card
+// Parker: Then you would take those dom nodes and append it to the document
